9311523348e5bcbfb801782944a33003767eeb46
Jose Paye Arroyo <jpaye@intelcomax.com>
2025-11-27 09:39:15 -0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'projectManager.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+
+export class AppModule {
+}
